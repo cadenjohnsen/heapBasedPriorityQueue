98773dfa41d6fcfe68a4c2573d0e22291a411bc0
Caden Johnsen <johnsenc@flip1.engr.oregonstate.edu>
2020-08-30 21:08:29 -0700

    Final Version

---
+++ pq.c
@@ -0,0 +1,162 @@
+/*
+ * In this file, you will write the structures and functions needed to
+ * implement a priority queue.  Feel free to implement any helper functions
+ * you need in this file to implement a priority queue.  Make sure to add your
+ * name and @oregonstate.edu email address below:
+ */
+
+#include <stdlib.h>
+
+#include "pq.h"
+#include "dynarray.h"
+#include <stdbool.h>
+
+/*
+ * This is the structure that represents a priority queue.  You must define
+ * this struct to contain the data needed to implement a priority queue.
+ */
+struct pq{
+	struct dynarray* value;
+	struct dynarray* priority;
+	int* temp;
+};
+
+/*
+ * This function should allocate and initialize an empty priority queue and
+ * return a pointer to it.
+ */
+struct pq* pq_create() {
+	struct pq* pq = malloc(sizeof(struct pq*));
+	pq->value = dynarray_create();
+	pq->priority = dynarray_create();
+	pq->temp = malloc(sizeof(int));
+	return pq;
+}
+
+/*
+ * This function should free the memory allocated to a given priority queue.
+ * Note that this function SHOULD NOT free the individual elements stored in
+ * the priority queue.  That is the responsibility of the caller.
+ *
+ * Params:
+ *   pq - the priority queue to be destroyed.  May not be NULL.
+ */
+void pq_free(struct pq* pq) {
+	dynarray_free(pq->value);
+	dynarray_free(pq->priority);
+	free(pq->temp);
+	free(pq);
+}
+
+/*
+ * This function should return 1 if the specified priority queue is empty and
+ * 0 otherwise.
+ *
+ * Params:
+ *   pq - the priority queue whose emptiness is to be checked.  May not be
+ *     NULL.
+ *
+ * Return:
+ *   Should return 1 if pq is empty and 0 otherwise.
+ */
+int pq_isempty(struct pq* pq) {
+	if(dynarray_size(pq->priority) != 0) {
+		return 0;
+	}
+	return 1;
+}
+
+/*
+ * This function should insert a given element into a priority queue with a
+ * specified priority value.  Note that in this implementation, LOWER priority
+ * values are assigned to elements with HIGHER priority.  In other words, the
+ * element in the priority queue with the LOWEST priority value should be the
+ * FIRST one returned.
+ *
+ * Params:
+ *   pq - the priority queue into which to insert an element.  May not be
+ *     NULL.
+ *   value - the value to be inserted into pq.
+ *   priority - the priority value to be assigned to the newly-inserted
+ *     element.  Note that in this implementation, LOWER priority values
+ *     should correspond to elements with HIGHER priority.  In other words,
+ *     the element in the priority queue with the LOWEST priority value should
+ *     be the FIRST one returned.
+ */
+void pq_insert(struct pq* pq, void* value, int priority) {
+	int index = 0;
+	//int* temp = malloc(sizeof(int));
+	int* tmp;
+	bool done = false;
+
+	for (int i = 0; i < dynarray_size(pq->priority); i++) {
+		if (done == false) {
+			tmp = dynarray_get(pq->priority, i);
+			if (priority < *tmp) {
+				//dynarray_insert(pq->priority, i, &priority);
+				index = i;
+				done = true;
+			}
+		}
+	}
+	*(pq->temp) = priority;
+	dynarray_insert(pq->priority, index, pq->temp);
+	dynarray_insert(pq->value, index, value);
+	////////////////free(temp);
+}
+
+/*
+ * This function should return the value of the first item in a priority
+ * queue, i.e. the item with LOWEST priority value.
+ *
+ * Params:
+ *   pq - the priority queue from which to fetch a value.  May not be NULL or
+ *     empty.
+ *
+ * Return:
+ *   Should return the value of the first item in pq, i.e. the item with
+ *   LOWEST priority value.
+ */
+void* pq_first(struct pq* pq) {
+	int *num;
+	num = dynarray_get(pq->value, 0);
+	return num;
+}
+
+/*
+ * This function should return the priority value of the first item in a
+ * priority queue, i.e. the item with LOWEST priority value.
+ *
+ * Params:
+ *   pq - the priority queue from which to fetch a priority value.  May not be
+ *     NULL or empty.
+ *
+ * Return:
+ *   Should return the priority value of the first item in pq, i.e. the item
+ *   with LOWEST priority value.
+ */
+int pq_first_priority(struct pq* pq) {
+	int *num;
+	num = dynarray_get(pq->priority, 0);
+	return *num;
+}
+
+/*
+ * This function should return the value of the first item in a priority
+ * queue, i.e. the item with LOWEST priority value, and then remove that item
+ * from the queue.
+ *
+ * Params:
+ *   pq - the priority queue from which to remove a value.  May not be NULL or
+ *     empty.
+ *
+ * Return:
+ *   Should return the value of the first item in pq, i.e. the item with
+ *   LOWEST priority value.
+ */
+void* pq_remove_first(struct pq* pq) {
+	void* i = dynarray_get(pq->value, 0);
+	dynarray_remove(pq->priority, 0);
+	dynarray_remove(pq->value, 0);
+	return i;
+}
